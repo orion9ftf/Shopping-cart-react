f06ae37f2df8b812634ff46da22a5781fbeb3b7d
Maypher92 <eva.matus@hotmail.com>
2025-07-12 18:39:13 -0400

    shoppig cart with react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <section class="hero" id="inicio">
+          <h1>Compra lo que necesites, en un solo lugar</h1>
+          <p>Carrito de compras</p>
+      </section>
     </div>
   );
 }
